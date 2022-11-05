c22d556d4d83847112beebbfa7684629c72acca8
Christopher <olanochristopher@gmail.com>
2022-11-05 13:11:25 +0800

    javascript prettier : use single quote

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll('a:link');
 
 allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
+  link.addEventListener('click', function (e) {
     e.preventDefault();
-    const href = link.getAttribute("href");
+    const href = link.getAttribute('href');
 
     // Scroll back to top
-    if (href === "#")
+    if (href === '#')
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
 
     // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
     }
 
     // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
   });
 });
 
 // Sticky navigation
 
-const sectionHeroEl = document.querySelector(".section-hero");
+const sectionHeroEl = document.querySelector('.section-hero');
 
 const callback = (entries) => {
   const ent = entries[0];
   console.log(ent);
 
   if (ent.isIntersecting === false) {
-    document.body.classList.add("sticky");
+    document.body.classList.add('sticky');
   }
 
   if (ent.isIntersecting === true) {
-    document.body.classList.remove("sticky");
+    document.body.classList.remove('sticky');
   }
 };
 
-const option = { root: null, threshold: 0, rootMargin: "-80px" };
+const option = { root: null, threshold: 0, rootMargin: '-80px' };
 
 const obs = new IntersectionObserver(callback, option);
 
@@ -52,9 +52,9 @@ obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
 });
